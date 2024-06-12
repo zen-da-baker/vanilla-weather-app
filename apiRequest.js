5fee7bcb4ff8937e585b793d3d4511d4e5055a72
Dawn <120154112+zen-da-baker@users.noreply.github.com>
2024-06-12 13:28:29 -0700

    7 day forcast in api request

--- apiRequest.js
+++ apiRequest.js
@@ -22,6 +22,48 @@ function renderResponse(res) {
         <p>Temp: ${periods[1].temperature} ${periods[1].temperatureUnit}</p>
         `;
 
+    document.getElementById("day2").innerHTML = 
+        `
+        <h2>${periods[2].name}</h2>
+        <p>${periods[2].detailedForecast}</p>
+        <p>Temp: ${periods[2].temperature} ${periods[2].temperatureUnit}</p>
+        `;
+
+    document.getElementById("day3").innerHTML = 
+        `
+        <h2>${periods[4].name}</h2>
+        <p>${periods[4].detailedForecast}</p>
+        <p>Temp: ${periods[4].temperature} ${periods[4].temperatureUnit}</p>
+        `;
+
+    document.getElementById("day4").innerHTML = 
+        `
+        <h2>${periods[6].name}</h2>
+        <p>${periods[6].detailedForecast}</p>
+        <p>Temp: ${periods[6].temperature} ${periods[6].temperatureUnit}</p>
+        `;
+
+        document.getElementById("day5").innerHTML = 
+        `
+        <h2>${periods[8].name}</h2>
+        <p>${periods[8].detailedForecast}</p>
+        <p>Temp: ${periods[8].temperature} ${periods[8].temperatureUnit}</p>
+        `;
+
+        document.getElementById("day6").innerHTML = 
+        `
+        <h2>${periods[10].name}</h2>
+        <p>${periods[10].detailedForecast}</p>
+        <p>Temp: ${periods[10].temperature} ${periods[10].temperatureUnit}</p>
+        `;
+
+        document.getElementById("day7").innerHTML = 
+        `
+        <h2>${periods[12].name}</h2>
+        <p>${periods[12].detailedForecast}</p>
+        <p>Temp: ${periods[12].temperature} ${periods[12].temperatureUnit}</p>
+        `;
+
 }
 
 const getData = async () => {
