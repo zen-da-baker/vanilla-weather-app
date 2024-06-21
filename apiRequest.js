d76f8a93632478fa1de427279fd0e3a6b0296011
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-06-21 08:05:37 -0700

    API response bug fix

--- apiRequest.js
+++ apiRequest.js
@@ -53,8 +53,12 @@ function renderResponse(res) {
         }
     }
 
+    /* 
     // display humidity stats by taking the index of the JSON response
+    // Property removed from JSON response by NOAA in June 2024
     function displayHumidity(i) {
+
+        return 0;
 
         // if the result is null, display a 0 instead
         if (periods[i].relativeHumidity.value == undefined || periods[i].relativeHumidity.value == null) {
@@ -64,30 +68,32 @@ function renderResponse(res) {
         }
     }
 
-    // Rain and humidity variables with values of the rain and humidity functions
-    let day1Rain = displayRain(0);
     let day1Humidity = displayHumidity(0);
+    let night1Humidity = displayHumidity(1);
+    let day2Humidity = displayHumidity(2);
+    let day3Humidity = displayHumidity(4);
+    let day4Humidity = displayHumidity(6);
+    let day5Humidity = displayHumidity(8);
+    let day6Humidity = displayHumidity(10);
+    let day7Humidity = displayHumidity(12);
+    */
+
+    // Rain variables with values of the rain and humidity functions
+    let day1Rain = displayRain(0);
 
     let night1Rain = displayRain(1);
-    let night1Humidity = displayHumidity(1);
 
     let day2Rain = displayRain(2);
-    let day2Humidity = displayHumidity(2);
 
     let day3Rain = displayRain(4);
-    let day3Humidity = displayHumidity(4);
 
     let day4Rain = displayRain(6);
-    let day4Humidity = displayHumidity(6);
 
     let day5Rain = displayRain(8);
-    let day5Humidity = displayHumidity(8);
 
     let day6Rain = displayRain(10);
-    let day6Humidity = displayHumidity(10);
 
     let day7Rain = displayRain(12);
-    let day7Humidity = displayHumidity(12);
 
     
     // render JSON response on manipulated DOM elements of the page
@@ -98,7 +104,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[0].temperature} ${periods[0].temperatureUnit}</p>
         <p>Winds: ${periods[0].windSpeed} ${periods[0].windDirection}</p>
         <p>Percipitation: ${day1Rain} %</p>
-        <p>Humidity: ${day1Humidity} %</p>
         `;
     
     document.getElementById("night1").innerHTML = 
@@ -108,7 +113,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[1].temperature} ${periods[1].temperatureUnit}</p>
         <p>Winds: ${periods[1].windSpeed} ${periods[1].windDirection}</p>
         <p>Percipitation: ${night1Rain} %</p>
-        <p>Humidity: ${night1Humidity} %</p>
         `;
 
     document.getElementById("day2").innerHTML = 
@@ -118,7 +122,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[2].temperature} ${periods[2].temperatureUnit}</p>
         <p>Winds: ${periods[2].windSpeed} ${periods[2].windDirection}</p>
         <p>Percipitation: ${day2Rain} %</p>
-        <p>Humidity: ${day2Humidity} %</p>
         `;
 
     document.getElementById("day3").innerHTML = 
@@ -128,7 +131,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[4].temperature} ${periods[4].temperatureUnit}</p>
         <p>Winds: ${periods[4].windSpeed} ${periods[4].windDirection}</p>
         <p>Percipitation: ${day3Rain} %</p>
-        <p>Humidity: ${day3Humidity} %</p>
         `;
 
     document.getElementById("day4").innerHTML = 
@@ -138,7 +140,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[6].temperature} ${periods[6].temperatureUnit}</p>
         <p>Winds: ${periods[6].windSpeed} ${periods[6].windDirection}</p>
         <p>Percipitation: ${day4Rain} %</p>
-        <p>Humidity: ${day4Humidity} %</p>
         `;
 
     document.getElementById("day5").innerHTML = 
@@ -148,7 +149,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[8].temperature} ${periods[8].temperatureUnit}</p>
         <p>Winds: ${periods[8].windSpeed} ${periods[8].windDirection}</p>
         <p>Percipitation: ${day5Rain} %</p>
-        <p>Humidity: ${day5Humidity} %</p>
         `;
 
     document.getElementById("day6").innerHTML = 
@@ -158,7 +158,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[10].temperature} ${periods[10].temperatureUnit}</p>
         <p>Winds: ${periods[10].windSpeed} ${periods[10].windDirection}</p>
         <p>Percipitation: ${day6Rain} %</p>
-        <p>Humidity: ${day6Humidity} %</p>
         `;
 
     document.getElementById("day7").innerHTML = 
@@ -168,7 +167,6 @@ function renderResponse(res) {
         <p>Temp: ${periods[12].temperature} ${periods[12].temperatureUnit}</p>
         <p>Winds: ${periods[12].windSpeed} ${periods[12].windDirection}</p>
         <p>Percipitation: ${day7Rain} %</p>
-        <p>Humidity: ${day7Humidity} %</p>
         `;
 
 }
